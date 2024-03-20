33727017ed4061275b68109dd9ad24798a012e3f
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-20 13:52:54 +0800

    add header

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 const MenuItem = ({ title, address, Icon}) => {
   return (
-    <Link href={ address }>
-        <Icon className="text-2xl"/>
-        <p className="">{title}</p>
+    <Link href={ address } className="hover:text-amber-500">
+        <Icon className="text-2xl sm:hidden"/>
+        <p className="uppercase hidden sm:inline text-sm">{title}</p>
     </Link>
   )
 }
