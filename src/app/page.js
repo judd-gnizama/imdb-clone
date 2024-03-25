097625121536c54c1743802433670db34aa5844b
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-25 09:42:30 +0800

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,8 @@ export default async function Home( {searchParams}) {
   const genre = searchParams.genre || 'fetchTrending' // if no genre specified, default is Trending
   const res = await fetch(
     `https://api.themoviedb.org/3/${
-      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/movie/day'}?api_key=${API_KEY}&language=en-US&page=1`
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/movie/day'}?api_key=${API_KEY}&language=en-US&page=1`, 
+      {next: {revalidate: 10000}}
     )
   if(!res.ok) {
     throw new Error('Failed to fetch dataeg')
